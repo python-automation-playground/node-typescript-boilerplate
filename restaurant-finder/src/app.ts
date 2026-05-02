c461fa4d87138f367ebc64c078d7da68aa0fd38e
unknown <prisatyanov29@gmail.com>
2026-05-02 16:40:29 +0530

    feat(update): update script to add dotenv

--- restaurant-finder/src/app.ts
+++ restaurant-finder/src/app.ts
@@ -1,9 +1,27 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+import { pool } from "./db";
 import express from "express";
 import restaurantRoutes from "./routes/restaurantRoutes";
 
 const app = express();
-
 app.use(express.json());
+
+(async () => {
+  try {
+    const res = await pool.query("SELECT * FROM restaurants");
+    console.log(" DB Connected:", res.rows);
+  } catch (err: any) {
+     console.error("FULL ERROR:", {
+    message: err.message,
+    code: err.code,
+    detail: err.detail,
+  });
+  }
+})();
 
 app.use("/restaurants", restaurantRoutes);
 
